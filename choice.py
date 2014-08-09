a522b2571e6bbb8391f9567dc420bc7f405e3b46
Jonathan Marmor <jmarmor@gmail.com>
2014-08-09 15:19:01 -0400

    Another input format for weighted choice.

--- choice.py
+++ choice.py
@@ -4,7 +4,11 @@ import random
 
 
 def weighted_choice(pairs):
-    """Choose an item from a list of (item, weight) pairs"""
+    """Choose an item from a list of (item, weight) pairs
+
+    >>> chords = [((0, 4, 7), 10), ((0, 4, 7), 5)]
+
+    """
     weights = [weight for item, weight in pairs]
     sum_of_weights = sum(weights)
     rand = random.uniform(0, sum_of_weights)
@@ -20,5 +24,26 @@ def dict_weighted_choice(d):
 
     Works for collections.Counter using the counts as weights.
 
+    >>> chords = {(0, 4, 7): 10, (0, 4, 7): 5}
+    >>> dict_weighted_choice(chords)
+    (0, 4, 7)
+
     """
     return weighted_choice(d.items())
+
+
+def list_of_dicts_weighted_choice(items, item_key='item', weight_key='weight'):
+    """Choose an item from a list of dicts where one of each dict's members is a weight.
+
+    >>> chords = [{'chord': (0, 4, 7), 'count': 10}, {'chord': (0, 4, 7), 'count': 5}]
+    >>> list_of_dicts_weighted_choice(chords, item_key='chord', weight_key='count')
+    (0, 4, 7)
+
+    """
+    pairs = [(i[item_key], i[weight_key]) for i in items]
+    return weighted_choice(pairs)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()

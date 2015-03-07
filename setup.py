df59fcc979c6f2f4050a70ff5e58ab39066d15f8
Jonathan Marmor <jmarmor@gmail.com>
2015-03-07 15:33:14 -0500

    Bump release number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jam',
-    version='0.0.1',
+    version='0.0.2',
     description="Jonathan's assorted music utilities",
     author='Jonathan Marmor',
     author_email='jmarmor@gmail.com',

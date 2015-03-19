f9475d68db9ddfaa467988f0378cd73e41b05842
Jonathan Marmor <jmarmor@gmail.com>
2015-03-18 22:56:06 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jam',
-    version='0.0.3',
+    version='0.0.4',
     description="Jonathan's assorted music utilities",
     author='Jonathan Marmor',
     author_email='jmarmor@gmail.com',
